8a3ade14b98eac34b6ececba6557e986c94e6121
Bryant6 <1963899588@qq.com>
2021-06-23 15:47:46 +0800

    Eureka单机环境搭建

--- cloud-provider-payment8001/src/main/java/com/wang/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/wang/springcloud/PaymentMain8001.java
@@ -2,12 +2,15 @@ package com.wang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangyu
  * @date 2021/6/17 18:48
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
