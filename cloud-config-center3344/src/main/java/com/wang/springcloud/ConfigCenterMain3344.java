67185132c5a7a00f99d35d504592d961728e2ab7
Bryant6 <1963899588@qq.com>
2021-06-30 16:14:25 +0800

    搭建Config分布式配置中心

---
+++ cloud-config-center3344/src/main/java/com/wang/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.wang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wangyu
+ * @date 2021/6/30 14:48
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-config-client3355/src/main/java/com/wang/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.wang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangyu
+ * @date 2021/6/30 15:45
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
+
